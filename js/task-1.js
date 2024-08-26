9911f8a64e9e6d6a207c58567f20188df1dedee8
Oleksii_Lobanov <alexeylobanov82@gmail.com>
2024-08-26 09:53:01 +0200

    add tasks 2-4, fix prettierrc

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ totalPrice = quantity * pricePerDroid;
 if (totalPrice > customerCredits) {
 return "Insufficient funds!";
  } else {
-     return "You ordered ${quantity} droids worth ${totalPrice} credits!"; 
+     return `You ordered ${quantity} droids worth ${totalPrice} credits!`; 
     }
  }
  console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
